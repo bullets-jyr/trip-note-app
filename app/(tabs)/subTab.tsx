a271b16cf261289ea09de9814ab9696d09af0a70
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 14:05:52 +0900

    프로젝트 구조 살펴보기, 엑스포 라우트 설명

---
+++ app/(tabs)/subTab.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const SubTabScreen = () => {
+    return (
+        <View>
+            <Text>SubTabScreen</Text>
+        </View>
+    );
+};
+
+export default SubTabScreen;
