43737a9761bd3d393c053cb8c9026fcf2aeeeaed
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 16:01:36 +0900

    루트 레이아웃 구성하기

--- app/(trip)/index.tsx
+++ app/(trip)/index.tsx
@@ -1,52 +1,11 @@
-import {SafeAreaView} from "react-native-safe-area-context";
-import {useDeferredValue, useMemo, useState, useTransition} from "react";
-import {FlatList, TextInput, Text} from "react-native";
-
-function slowFilter(text: string) {
-    const result: string[] = [];
-    for (let i = 0; i < 1500; i++) {
-        result.push(`${text} - item ${i}`);
-    }
-    return result;
-}
+import { theme } from "@/constants/theme";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const MyTripList = () => {
-    const [textValue, setInputValue] = useState("");
-    // const [items, setItems] = useState<string[]>([]);
-    // const [isPending, startTransition] = useTransition();
-    const deferredText = useDeferredValue(textValue);
-
-    // const handleChange = (value: string) => {
-    //     setInputValue(value);
-    //     startTransition(() => {
-    //         setItems(slowFilter(value));
-    //     });
-    // };
-
-    const items = useMemo(() => {
-        if (!deferredText) return [];
-        return slowFilter(deferredText);
-    }, [deferredText])
-
-    const isPending = textValue !== deferredText;
-
     return (
         <SafeAreaView>
-            <TextInput
-                value={textValue}
-                onChangeText={setInputValue}
-                style={{borderWidth: 1, height: 50}}
-            />
-            {isPending && (
-                <Text style={{color: "grey"}}>리스트 업데이트 중...</Text>
-            )}
-            <FlatList
-                data={items}
-                keyExtractor={(item) => item}
-                renderItem={({item}) => (
-                    <Text style={{opacity: isPending ? 0.5 : 1}}>{item}</Text>
-                )}
-            />
+            <Text style={{ fontFamily: theme.fonts.bold }}>폰트 적용 확인</Text>
         </SafeAreaView>
     );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,46 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import {SplashScreen, Stack} from 'expo-router';
+import {StatusBar} from 'expo-status-bar';
 import 'react-native-reanimated';
+import {useEffect} from "react";
+import {useFonts} from "expo-font";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
-  anchor: '(trip)',
+    anchor: '(trip)',
 };
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 3,
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 5,
+        },
+    },
+});
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(trip)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        "Pretendart-Regular": require("../assets/fonts/Pretendard-Regular.otf"),
+        "Pretendart-Medium": require("../assets/fonts/Pretendard-Medium.otf"),
+        "Pretendart-SemiBold": require("../assets/fonts/Pretendard-SemiBold.otf"),
+        "Pretendart-Bold": require("../assets/fonts/Pretendard-Bold.otf"),
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Stack>
+                <Stack.Screen name="(trip)" options={{headerShown: false}}/>
+            </Stack>
+            <StatusBar style="auto"/>
+        </QueryClientProvider>
+    );
 }
