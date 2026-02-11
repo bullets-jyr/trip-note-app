42b0d0f75e6330977922d4864e7768cd5e24431a
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 14:57:27 +0900

    React 18 Concurrency 개념과 UI 영향(Suspense)

--- app/(trip)/index.tsx
+++ app/(trip)/index.tsx
@@ -1,19 +1,54 @@
-import {Text, TextInput} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {useState} from "react";
+import {ActivityIndicator, Text, View} from "react-native";
+import {Suspense} from "react";
 
 const MyTripList = () => {
-    const [inputValue, setInputValue] = useState([]);
+    let userInfo: { name: String; email: String } | null = null;
+    let userPromise: Promise<void> | null = null;
+
+    function fetchUser() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                userInfo = {
+                    name: 'bullets',
+                    email: 'jy.ryu.jiyoung@gmail.com',
+                };
+                resolve();
+            }, 2000)
+        })
+    }
+
+    function useUser() {
+        if (userInfo) {
+            return userInfo;
+        }
+        if (!userPromise) {
+            userPromise = fetchUser();
+        }
+
+        throw userPromise;
+    }
+
+    function UserProfile() {
+        const user = useUser();
+        return (
+            <View>
+                <Text>{user.name}</Text>
+                <Text>{user.email}</Text>
+            </View>
+        )
+    }
+
     return (
-        <SafeAreaView>
-            <TextInput
-                keyboardType="email-address"
-                value={inputValue}
-                style={{width: 200, height: 40, borderWidth: 1}}
-                onChangeText={(text: string) => setInputValue(text)}
-            />
-            <Text>{inputValue}</Text>
-        </SafeAreaView>
+        <Suspense fallback={
+            <View style={{flex: 1, justifyContent: 'center'}}>
+                <ActivityIndicator size="large"/>
+            </View>
+        }>
+            <SafeAreaView>
+                <UserProfile/>
+            </SafeAreaView>
+        </Suspense>
     );
 };
 
